b71c1f8d817b708076da0983d39d25c87f1bc726
Yizhi Zhang <yzhang97@hawk.iit.edu>
2020-06-22 20:15:53 -0500

    complete ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,19 +1,68 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+        This function reads messages.csv from messages_filepath and 
+        categories.csv from categories_filepath. Then merge them in to a datafrom df
+
+        Input:  messages_filepath - message.csv filepath
+                categories_filepath - categories.csv filepath
+        
+        Output: df
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, left_on='id', right_on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    This function corrects the names of category columns 
+    and assign integer values accordingly.
+
+    Input:  df
+    Output: df
+    '''
+    categories = df['categories'].str.split(';',expand=True)
+    categories.columns = categories.iloc[0].apply(lambda x: x.split('-')[0])
+    
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x.split('-')[-1]).apply(int)
+
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories],axis=1)
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    This function loads df to database_filename.db under current folder.
+
+    Input:  df - dataframe
+            database_filename name of .db file
+
+    Output: None
+    '''
+    engine = create_engine('sqlite:///DisasterResponse.db')
+    df.to_sql('df', engine, index=False)
 
 
 def main():
+    """
+    Main Data Processing function
+    
+    This function implement the ETL pipeline:
+        1) Data extraction from .csv
+        2) Data cleaning and processing
+        3) Data loading to SQLite database
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]

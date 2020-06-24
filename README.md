2f1b0d05ae19758b2d479be21236fc9f7d8fd337
Yizhi Zhang <yzhang97@hawk.iit.edu>
2020-06-23 19:37:41 -0500

    resubmit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# DisasterResponsePipelines
-This is an individual project to create ETL and ML pipelines to process messaging data during disaster time.
+# Disaster Response Pipeline Project
+
+I created a disaster response pipeline to learn from existing data and categorize new messages to appropriate categories.
+
+### Folder and Files
+Folder - APP:
+    Folder - templates:
+        go.html - result page.
+        master.html - home page.
+    run.py - python script that loads database from data folder and model from models folder. Then it generate to webpage on a local host.
+
+Folder - data:
+    disaster_categories.csv - file containing categorized data.
+    disaster_messages.csv - file containing raw messages.
+    DisasterResponse.db - database containing processed data for machine learning.
+    process_data.py - script that cleans and tokenizes data.
+
+Folder - models:
+    classifier.pkl - pickle file generated after machine learning process. It contains the model.
+    train_classifier.py - script that loads database from data folder and performs machine learning.
+
+README.md - this file
+screenshot1.png - a screenshot of the local web page
+
+### Required libraries
+nltk 3.3.0+
+numpy 1.15.2+
+pandas 0.23.4+
+scikit-learn 0.20.0+
+sqlalchemy 1.2.12+
+
+### Data Source
+
+This is a project guided by Udacity. The data came from Figure Eight.
---
+++ data/process_data.py
@@ -0,0 +1,92 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    '''
+        This function reads messages_filepath and categories_filepath. 
+        Then merge them in to a datafrom df
+
+        Input:  messages_filepath - message.csv filepath
+                categories_filepath - categories.csv filepath
+        
+        Output: df - dataframe containing uncleaned dataset
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, left_on='id', right_on='id')
+    return df
+
+
+def clean_data(df):
+    '''
+    This function corrects the names of category columns 
+    and assign integer values accordingly.
+
+    Input:  df - dataframe containing uncleaned dataset
+    Output: df - dataframe containing cleaned dataset
+    '''
+    categories = df['categories'].str.split(';',expand=True)
+    categories.columns = categories.iloc[0].apply(lambda x: x.split('-')[0])
+    
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x.split('-')[-1]).apply(int)
+
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories],axis=1)
+    df.drop_duplicates(inplace=True)
+
+    return df
+
+
+def save_data(df, database_filename):
+    '''
+    This function loads df to database_filename.db under current folder.
+
+    Input:  df - dataframe
+            database_filename name of .db file
+
+    Output: None
+    '''
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('df', engine, index=False)
+
+
+def main():
+    """
+    Main Data Processing function
+    
+    This function implement the ETL pipeline:
+        1) Data extraction from .csv
+        2) Data cleaning and processing
+        3) Data loading to SQLite database
+    """
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
